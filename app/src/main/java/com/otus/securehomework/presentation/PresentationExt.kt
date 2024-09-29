81c2a38c6ec66babea7e753dc3e31b787f0e6428
Robilko Aleksandr <sasha.robson@gmail.com>
2024-09-29 19:05:19 +0300

    Задание 2. Реализован вход используя биометрию

--- app/src/main/java/com/otus/securehomework/presentation/PresentationExt.kt
+++ app/src/main/java/com/otus/securehomework/presentation/PresentationExt.kt
@@ -1,8 +1,10 @@
 package com.otus.securehomework.presentation
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.snackbar.Snackbar
@@ -58,6 +60,7 @@ fun Fragment.handleApiError(
             R.string.error_internet,
             retry
         )
+
         failure.errorCode == 401 -> {
             if (this is LoginFragment) {
                 requireView().snackbar(R.string.error_enter)
@@ -65,9 +68,13 @@ fun Fragment.handleApiError(
                 logout()
             }
         }
+
         else -> {
             val error = failure.errorBody?.string().toString()
             requireView().snackbar(error)
         }
     }
 }
+
+fun Context.showToastShort(message: String) =
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
